fc8f4ec925bd4e79ac83098890f38936fd17e41d
Pierre Antoine <mbefokam@gmail.com>
2018-04-14 10:27:32 -0400

    I have added the static login

--- src/components/public/common/Header.js
+++ src/components/public/common/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import { Navbar,Nav,NavItem }from  'react-bootstrap';
 
 import Home from '../home/HomePage';
 import About from '../about/AboutPage';
 import Contact from  '../contact/ContactPage';
-import { Navbar,Nav,NavItem }from  'react-bootstrap';
+import Login from '../login/Login';
 import './Header.css'; 
 
  
@@ -26,17 +27,16 @@ const Header = () => {
        < Link to="/"  >Home</ Link>
       </NavItem>
       <NavItem eventKey={2}>
-       < Link to="/about"  >About</ Link>
+       < Link to="/about">About</ Link>
       </NavItem>
-      <NavItem eventKey={3} href="#">
-      < Link to="/contact"  >Contact
-      
-      </ Link>
+      <NavItem eventKey={3}>
+      < Link to="/contact">Contact</ Link>
       </NavItem>
     </Nav>
     <Nav pullRight>
       <NavItem eventKey={1} href="#">
-        Login
+      {/* < Link to="/login">Login</ Link> */}
+      <Login />
       </NavItem>  
       <NavItem eventKey={2} href="#">
         Sign up
@@ -46,10 +46,8 @@ const Header = () => {
 </Navbar>
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} 
-       
-      
-      />
+      <Route path="/contact" component={Contact} />
+      <Route path="/login" component={Login} />     
       </div>
     );
 };
---
+++ src/components/public/login/Login.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+// import classNames from 'classnames';
+import 'typeface-roboto';
+import TextField from 'material-ui/TextField';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from 'material-ui/Dialog';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import IconButton from 'material-ui/IconButton';
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+import { FormControl} from 'material-ui/Form';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Select from 'material-ui/Select';
+import Typography from 'material-ui/Typography';
+
+import {NavItem }from  'react-bootstrap';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  withoutLabel: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  textField: {
+    flexBasis: 200,
+  },
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120
+  }
+});
+
+// const categories = [
+//   {
+//     value: 'patient',
+//     label: 'Patient',
+//   },
+//   {
+//     value: 'institution',
+//     label: 'Institution',
+//   },
+//   {
+//     value: 'provider',
+//     label: 'Provider',
+//   },
+// ];
+
+class FormDialog extends React.Component {
+  state = {
+    open: false,
+    password: '',
+    showPassword: false,
+    category: "",
+  };
+
+  handleChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
+
+  handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  handleClickShowPassword = () => {
+    this.setState({ showPassword: !this.state.showPassword });
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <NavItem onClick={this.handleClickOpen}>Login</NavItem>
+        <Dialog
+          aria-labelledby="form-dialog-title"
+          disableBackdropClick
+          disableEscapeKeyDown
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <DialogTitle >
+          <Typography variant="headline" gutterBottom>
+          Login or Sign up to your profile CarnetHealth Inc.
+          </Typography>
+            </DialogTitle>
+          <DialogContent>
+            <FormControl className={styles.formControl}>
+                <InputLabel htmlFor="name-simple">Categories</InputLabel>
+                <Select
+                  value={this.state.category}
+                  onChange={this.handleChange("category")}
+                  input={<Input id="name-simple" />}
+                >
+                  <MenuItem value={"Patient"}>
+                  <Typography variant="headline" gutterBottom>
+                        Patient
+                   </Typography>
+                  </MenuItem>
+                  <MenuItem value={"Institution"}>
+                  <Typography variant="headline" gutterBottom>
+                       Institution
+                   </Typography>
+                  </MenuItem>
+                  <MenuItem value={"Provider"}>
+                  <Typography variant="headline" gutterBottom>
+                        Provider
+                   </Typography>
+                  </MenuItem>
+                </Select>
+              </FormControl>
+
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email or CarnetHealth ID"
+              type="email"
+              fullWidth
+            />
+            
+        <FormControl styles={{margin: "spacing.unit",flexBasis: 200}}>
+          <InputLabel htmlFor="adornment-password">Password</InputLabel>
+          <Input
+            id="adornment-password"
+            type={this.state.showPassword ? 'text' : 'password'}
+            value={this.state.password}
+            onChange={this.handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                  onMouseDown={this.handleMouseDownPassword}
+                >
+                  {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+                </InputAdornment>
+            }
+          />
+        </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Sign up
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Login
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+FormDialog.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(FormDialog);
